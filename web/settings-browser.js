775a2a8e80295ebdbe5029149224882693c4e882
moritzbrantner <31051084+moritzbrantner@users.noreply.github.com>
2026-09-16 10:51:16 +0200

    Add stable settings browser bridge

---
+++ web/settings-browser.js
@@ -0,0 +1,45 @@
+import initSettingsWasm, { SettingsSession } from './pkg/settings_wasm.js';
+
+export const SETTINGS_BROWSER_API_VERSION = 1;
+
+let initialization;
+
+export async function createSettingsSession(definitions) {
+  initialization ??= initSettingsWasm();
+  await initialization;
+  return new BrowserSettingsSession(
+    new SettingsSession(JSON.stringify(definitions)),
+  );
+}
+
+class BrowserSettingsSession {
+  #inner;
+
+  constructor(inner) {
+    this.#inner = inner;
+  }
+
+  effectiveValues() {
+    return JSON.parse(this.#inner.effective_values_json());
+  }
+
+  set(id, value) {
+    this.#inner.set_json(id, JSON.stringify(value));
+  }
+
+  reset(id) {
+    this.#inner.reset(id);
+  }
+
+  importScope(scope, snapshot) {
+    return JSON.parse(this.#inner.import_scope_json(scope, snapshot));
+  }
+
+  exportScope(scope) {
+    return this.#inner.export_scope_json(scope);
+  }
+
+  dispose() {
+    this.#inner.free();
+  }
+}
